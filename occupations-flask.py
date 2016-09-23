fe8ffbefe40ab5d5e71de273c897264452a7a810
root <root@Daniel-PC.fios-router.home>
2016-09-22 21:23:47 -0400

    did occupation-flask

--- occupations-flask.py
+++ occupations-flask.py
@@ -1,10 +1,22 @@
 from flask import Flask
 
+#################################################
+#code for csv file
+a = open('occupations.csv')
+a.readline()
+name = []
+perc = []
+for line in a:
+  b = line.rfind(",")
+  name.append(line[0:b])
+  perc.append((line.replace("\n",""))[b+1:])
+##################################################
+
 app= Flask(__name__)
 
-@app.route("/")
+@app.route("/occupations")
 def template():
-    return render_template();
+    return render_template('occupation-html.html',ocupation=name, percents=perc)
     
 if __name__ == '__main__':
     app.debug= True
