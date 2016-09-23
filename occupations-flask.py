8e4feaee3eb42dcf19dc073678b75502ef32d9d0
chiudaniels <daniel.sean.chiu@gmail.com>
2016-09-23 12:34:11 -0400

    changed to dictionary. doesnt work

--- occupations-flask.py
+++ occupations-flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #################################################
 #code for csv file
@@ -10,13 +10,14 @@ for line in a:
   b = line.rfind(",")
   name.append(line[0:b])
   perc.append((line.replace("\n",""))[b+1:])
+dictionary=dict (zip(name,perc))
 ##################################################
-
+print name
 app= Flask(__name__)
 
 @app.route("/occupations")
 def template():
-    return render_template('occupation-html.html',ocupation=name, percents=perc)
+    return render_template('occupations-html.html',ocupation=dictionary)
     
 if __name__ == '__main__':
     app.debug= True
