0fc2fcbcda137c6f215a389235d7d1f1b78616fb
chiudaniels <chiudaniels@gmail.com>
2016-09-25 16:17:15 -0400

    added random occ

--- occupations-flask.py
+++ occupations-flask.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import random
 #################################################
 #code for csv file
 a = open('occupations.csv')
@@ -11,13 +11,24 @@ for line in a:
   name.append(line[0:b])
   perc.append((line.replace("\n",""))[b+1:])
 dictionary=dict (zip(name,perc))
+del name[-1]
+del perc[-1]
+mess = []
+d = 0
+while d < len(name):
+  c = float(perc[d]) * 10
+  while c > 0:
+    c = c - 1
+    mess.append(name[d])
+  d = d + 1
+e = random.choice(mess)
 ##################################################
 #print name
 app= Flask(__name__)
 
 @app.route("/occupations")
 def template():
-    return render_template('occupations-html.html',occupation=dictionary)
+    return render_template('occupations-html.html',occupation=dictionary, random_occ=e)
     
 if __name__ == '__main__':
     app.debug= True
