2159535d208126bb51d0fb9c1384252949e7982c
chiudaniels <daniel.sean.chiu@gmail.com>
2016-09-27 12:35:18 -0400

    localized files

--- occu-table/occupations-flask.py
+++ occu-table/occupations-flask.py
@@ -1,34 +1,12 @@
 from flask import Flask, render_template
 import random
-#################################################
-#code for csv file
-a = open('occupations.csv')
-a.readline()
-name = []
-perc = []
-for line in a:
-  b = line.rfind(",")
-  name.append(line[0:b])
-  perc.append((line.replace("\n",""))[b+1:])
-dictionary=dict (zip(name,perc))
-del name[-1]
-del perc[-1]
-mess = []
-d = 0
-while d < len(name):
-  c = float(perc[d]) * 10
-  while c > 0:
-    c = c - 1
-    mess.append(name[d])
-  d = d + 1
-e = random.choice(mess)
-##################################################
-#print name
+import occupation
+
 app= Flask(__name__)
 
 @app.route("/occupations")
 def template():
-    return render_template('occupations-html.html',occupation=dictionary, random_occ=e)
+    return render_template('occupations-html.html',occupation=occupation.getDict(), random_occ=occuapation.getRandom())
     
 if __name__ == '__main__':
     app.debug= True
