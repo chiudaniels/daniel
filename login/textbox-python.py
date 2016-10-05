21d49d5cde48e4d293025922125edfd12a490006
chiudaniels <chiudaniels@gmail.com>
2016-10-04 21:22:10 -0400

    doesnt work. view function error

--- login/textbox-python.py
+++ login/textbox-python.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request
+import csv, hashlib
 
 app = Flask(__name__)
+
+datalist={}
+
+def opendata():
+    with open('loginData.csv', 'rb') as csvfile:
+        data=csv.DictReader(csvfile)
+        datalist= dict(data)
 
 @app.route("/")
 def template():
@@ -8,16 +16,26 @@ def template():
 
 @app.route("/authenticate/", methods=['POST'])
 def worked():
-    if request.form['username'] == 'daniel' and request.form['password'] == 'chiu':
-        return "hello"
-    else:
-        return "didn't work"
+    opendata()
+    if request.form['submit'] == 'register':
+        if datalist.has_key(request.form['username']):
+            return render_template('textbox.html', message = 'username already exsists')
+        else:
+            with open('loginData.csv','wb') as csvfile:
+                fieldname=['username','password']
+                cred=csv.DictWriter(csvfile, fieldnames=fieldname)
+                password= hashlib.sha512(request.form['password'])
+                cred.writeheader()
+                cred.writerow({'username':request.form['username'], 'password':password.hexdigest()})
+                return render_template('textbox.html', message = 'username and password succesfully registered')
+    elif request.form['submit'] == 'login':
+        if datalist.has_key(request.form['username']):
+            if datalist.get(request.form['username']) == hashlib.sha512(request.form['password']).hexdigest():
+                return render_template('worked.html')
+            else:
+                return render_template('textbox.html', message='invalid username or password') 
 
-@app.route("/failed/")
-def didntWork():
-    return render_template('failure.html')
-
-    
 if __name__ == '__main__':
     app.debug = True
+    opendata()
     app.run()
